ad2fd9da34aca2c74b6c4cc55679df1b01c7affc
Aroddn <bessenyei.adam2001@gmail.com>
2024-07-19 00:38:02 +0200

    Main project first commit

---
+++ Assets/Scripts/SO Asset Scripts/CardAsset/CardAsset.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+
+
+public class CardAsset : ScriptableObject 
+{
+    [Header("General info")]
+    //public CharacterAsset characterAsset;
+    public Rarity rarity;
+    public Color color;
+    public int releaseYear;
+
+    //public bool reaction
+    [TextArea(2,3)]
+    public string Description;
+	public Sprite CardImage;
+    public int ManaCost;
+
+
+
+    [Header("Creature Info")]
+    public int MaxHealth;
+    public int Attack;
+    public int AttacksForOneTurn = 1;
+    public bool Taunt;
+    public bool Flying;
+    public string CreatureScriptName;
+    public int specialCreatureAmount;
+    public Type type;
+    public SubType subType;
+
+    
+
+    [Header("SpellInfo")]
+    public string SpellScriptName;
+    public int specialSpellAmount;
+    public PlaySpeed playSpeed;
+    public TargetingOptions Targets;
+
+}
+
+public enum TargetingOptions
+{
+    NoTarget,
+    AllCreatures,
+    EnemyCreatures,
+    YourCreatures,
+    AllCharacters,
+    EnemyCharacters,
+    YourCharacters
+}
+
+public enum Rarity
+{
+    COMMON,
+    UNCOMMON,
+    RARE,
+    ULTRARARE
+}
+
+public enum Color
+{
+    CHARADIN,
+    DORNODON,
+    FAIRLIGHT,
+    LEAH,
+    RAIA,
+    RHATT,
+    SHERAN,
+    THARR,
+    ELEINOS,
+    NONE
+}
+
+public enum PlaySpeed
+{
+    INSTANT,
+    GENERAL,
+    PISHOGUE,
+    REACTION
+}
+
+
+public enum Type
+{
+    MONSTER,
+    ADVENTURER
+}
+
+public enum SubType
+{
+    NONE,
+    ORK,
+    DRAGON,
+    PEGASUS,
+    PHOENIX,
+    GNOME,
+    DRAGON_PHOENIX,
+    SNAKE,
+    GRIFF,
+    ELF,
+    MUTANT
+}
+
+public enum Special
+{
+    FIRE,
+    LIGHTNING,
+    HAND,
+    MONSTER_COMPONENT,
+    ANCHOR,
+    MENTAL
+
+}
---
+++ Assets/Scripts/Visual/ManaPoolVisual.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+
+[ExecuteInEditMode]
+public class ManaPoolVisual : MonoBehaviour {
+
+    public int currentMana;
+    public int maxMana = 20;
+    
+    private int totalCrystals;
+    public int TotalCrystals
+    {
+        get{ return totalCrystals; }
+
+        set
+        {
+            totalCrystals = value;
+            ProgressText.text = string.Format("{0}/{1}", availableCrystals.ToString(), totalCrystals.ToString());
+        }
+    }
+
+    private int availableCrystals = 20;
+    public int AvailableCrystals
+    {
+        get{ return availableCrystals; }
+
+        set
+        {
+            if(value > totalCrystals)
+                availableCrystals = totalCrystals;
+            else if (value < 0)
+                availableCrystals = 0;
+            else
+                availableCrystals = value;
+            ProgressText.text = string.Format("{0}/{1}", availableCrystals.ToString(), totalCrystals.ToString());
+        }
+    }
+
+    public TMP_Text ProgressText;
+
+    void Update()
+    {
+        if (Application.isEditor && !Application.isPlaying)
+        {
+            AvailableCrystals = currentMana;
+            TotalCrystals = maxMana;
+        }
+    }
+
+}
